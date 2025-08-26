468b4ccdf17e1f0f25c1f9cebb856be92cd4625e
José Lin <linjose@users.noreply.github.com>
2025-08-26 23:56:27 +0800

    Create create_table_judbp.sql

---
+++ judbp/create_table_judbp.sql
@@ -0,0 +1,14 @@
+CREATE TABLE judbp (
+
+court_name TEXT,
+share_type TEXT,
+auction_date_times TEXT,
+county TEXT,
+address_area TEXT,
+auction_reserve_price INT,
+delivery_location TEXT,
+vacant_property TEXT,
+bid_type TEXT,
+remarks TEXT,
+bid_by_post TEXT
+);
